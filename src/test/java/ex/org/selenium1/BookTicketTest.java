4a53e79697cf4a7401f1903ef72e35577739a25c
A06438_P5.Training <A06438_P5.Training@P5-GL5L882.dir.svc.accenture.com>
2018-06-25 17:58:28 +0530

    flight booking

--- src/test/java/ex/org/selenium1/BookTicketTest.java
+++ src/test/java/ex/org/selenium1/BookTicketTest.java
@@ -1,8 +1,11 @@
 package ex.org.selenium1;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
@@ -19,9 +22,49 @@ public class BookTicketTest {
 		testing1.uname.sendKeys(uname);
 		testing1.password.sendKeys(pass);
         testing1.Login_button.click();
-        
+        Assert.assertEquals("Find a Flight:Mercury", driver.getTitle());
 	}
 	
+	@Test(priority=2)
+	public void findflight()
+	{
+		 
+		driver.findElement(By.cssSelector("input[name='tripType'][value='oneway']")).click();
+		
+		Select passengers = new Select(driver.findElement(By.name("passCount")));
+		passengers.selectByValue("2");
+		
+		Select DepartingFrom = new Select(driver.findElement(By.name("fromPort")));
+		DepartingFrom.selectByValue("London");
+		
+		Select On = new Select(driver.findElement(By.name("fromMonth")));
+		On.selectByValue("7");
+		
+		Select date = new Select(driver.findElement(By.name("fromDay")));
+		date.selectByValue("10");
+		
+		Select ArrivingIn = new Select(driver.findElement(By.name("toPort")));
+		ArrivingIn.selectByValue("Frankfurt");
+		
+		Select Returning = new Select(driver.findElement(By.name("toMonth")));
+		Returning.selectByValue("July");
+		
+		Select date1 = new Select(driver.findElement(By.name("toDay")));
+		date1.selectByValue("15");
+
+		
+		driver.findElement(By.cssSelector("input[name='servClass'][value='Business']")).click();
+		
+		Select Airline = new Select(driver.findElement(By.name("airline")));
+		Airline.selectByValue("Blue Skies Airlines");
+		
+		driver.findElement(By.name("findFlights")).click();
+		
+				
+		
+				
+	}
+
 	@DataProvider(name="logindata")
 	public String[][] login_data() throws Exception {
 		ExcelUtility.setExcelPath("Sheet1", "C:\\Users\\A06438_P5.Training\\Desktop\\Selenium Drivers\\SeleniumTestData.xlsx");
